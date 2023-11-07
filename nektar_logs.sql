57e28a1e63ff21c54d9aa292911914c9e0a2c96b
Lokeshwar Reddy Jonnalagadda <10lokeshnani@gmail.com>
2023-11-07 15:19:14 +0530

    Add SQL File for DB

---
+++ nektar_logs.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 07, 2023 at 10:47 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nektar_logs`
+--
+CREATE DATABASE IF NOT EXISTS `nektar_logs` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `nektar_logs`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs_cache`
+--
+
+CREATE TABLE `logs_cache` (
+  `log_time` datetime(3) NOT NULL DEFAULT current_timestamp(3),
+  `bytes` bigint(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `logs_cache`
+--
+ALTER TABLE `logs_cache`
+  ADD PRIMARY KEY (`log_time`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
